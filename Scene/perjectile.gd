7a3cccaab3b510b3813d76bfbb0aa6c11781db97
Sunny0714 <suhan@s.sfusd.edu>
2026-04-28 20:30:13 -0700

    projectile

--- Scene/perjectile.gd
+++ Scene/perjectile.gd
@@ -2,34 +2,40 @@ extends Area2D
 
 signal hit
 
-var rotate_speed : float = 3
+var rotate_speed: float = 3
 
-@export var move_direction : Vector2
-@export var move_speed : float = 50
-@export var wait_time : float
+@export var move_direction: Vector2
+@export var move_speed: float = 50
+@export var wait_time: float = 10
 
-@onready var start_pos : Vector2 = global_position
-@onready var target_pos : Vector2 = global_position + move_direction
+@onready var start_pos: Vector2 = global_position
+@onready var target_pos: Vector2 = global_position + move_direction
+@onready var sprite: Sprite2D = $Sprite
 
-@onready var sprite : Sprite2D = $Sprite
+var can_move := false
+
+func _ready() -> void:
+	await get_tree().create_timer(wait_time).timeout
+	can_move = true
 
 func _physics_process(delta):
 	var time = Time.get_unix_time_from_system()
-	
 	sprite.scale.x = sin(time * rotate_speed)
-	await get_tree().create_timer(wait_time).timeout
-	global_position = global_position.move_toward(target_pos, move_speed * delta)
-
+	if can_move:
+		global_position = global_position.move_toward(target_pos, move_speed * delta)
 
 func _on_body_entered(body):
 	if body.is_in_group("Player"):
 		_move_boss()
-	if body.is_in_group("Boss"):
-		emit_signal("hit")
-		queue_free()
-		
-		
+
 func _move_boss():
-	move_speed = 1000
-	global_position = global_position.move_toward(Vector2(300,-100), move_speed)
-	
+	var tween = create_tween()
+	tween.tween_property(self, "global_position", Vector2(200, -150), 0.4)
+
+
+
+
+func _on_enemy_boss_area_entered(area: Area2D) -> void:
+	emit_signal("hit")
+	await get_tree().create_timer(0.01).timeout
+	queue_free()
--- enemy_boss.gd
+++ enemy_boss.gd
@@ -11,14 +11,16 @@ signal stop
 
 func _ready (): 
 	$AnimationPlayer.play("fly")
-	var sender = get_node("/Perjectile")
-	if sender:
-		sender.connect("hit", Callable(self, "_hit1"))
+	for p in get_tree().get_nodes_in_group("Perjectile"):
+		p.connect("hit", Callable(self, "_hit"))
 
 func _physics_process(delta):
+	get_tree().call_group("Player", "set_physics_process", false)
 	await get_tree().create_timer(2).timeout
 	global_position = global_position.move_toward(target_pos, move_speed * delta)
 	emit_signal("stop")
+	await get_tree().create_timer(5).timeout
+	get_tree().call_group("Player", "set_physics_process", true)
 
 func _on_body_entered(body):
 	if not body.is_in_group("Player"):
@@ -26,7 +28,7 @@ func _on_body_entered(body):
 	
 	body.take_damage(3)
 
-func _hit1():
+func _hit():
 	sprite.modulate = Color.RED
-	await get_tree().create_timer(0.05).timeout
+	await get_tree().create_timer(0.1).timeout
 	sprite.modulate = Color.WHITE
